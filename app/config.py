0e7d62a234ce5b0852f565e36b3d10d102a1d77c
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2024-04-13 12:28:21 -0500

    FIX: Project structure :building_construction:

---
+++ app/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
--- static/js/clear_inputs.js
+++
@@ -1,5 +0,0 @@
-let send_button = document.getElementById('send').addEventListener('click', (event) => {
-    setTimeout(() => {
-        document.location.reload();
-    }, 3000);
-})
